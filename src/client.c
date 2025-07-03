61c748436e828cf9bf6d9cf8627a11c79ad642e9
Joshua <joshuasantosa@proton.me>
2025-07-03 16:40:34 +1000

    Added SIGINT and server disconnect catching for client

--- src/client.c
+++ src/client.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <errno.h>
 #ifdef _WIN32
   #include <winsock2.h>
   #include <ws2tcpip.h>
@@ -41,7 +42,7 @@ int main(int argc, char *argv[]) {
 
   fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd == -1) {
-    printf("Kappa Chungus this is not netWORKING\n");
+    perror("Kappa Chungus this is not netWORKING");
     exit(EXIT_FAILURE);
   }
 
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]) {
   addr.sin_family = AF_INET;
   addr.sin_port = htons(8080);
   if (inet_pton(AF_INET, argAddr, &addr.sin_addr) <= 0) {
-    printf("Invalid address\n");
+    perror("Invalid address");
     exit(EXIT_FAILURE);
   }
 
@@ -57,7 +58,7 @@ int main(int argc, char *argv[]) {
   printf("Trying to connect to server\n");
   socklen_t addrlen = sizeof(addr);
   if (connect(fd, (struct sockaddr *) &addr, addrlen) == -1) {
-    printf("OOFT cannot connect\n");
+    perror("OOFT cannot connec");
     CLOSE(fd);
     exit(EXIT_FAILURE);
   }
@@ -65,7 +66,17 @@ int main(int argc, char *argv[]) {
   printf("Connected to %s on port 8080\n", argAddr);
 
   // Signal Handling
-  signal(SIGINT, handle_sigint);
+  // TODO: see if we can make windows interrupt fgets
+  #ifdef _WIN32
+    signal(SIGINT, handle_sigint);
+  #else
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+  #endif
 
   // Write data to server
   char buffer[1024];
@@ -73,8 +84,8 @@ int main(int argc, char *argv[]) {
   while (!stop) {
     printf("Enter message: ");
     if (!fgets(buffer, sizeof(buffer), stdin)) {
-      if (stop) break;
-      printf("Wadafuq fgets error!?!");
+      if (stop || errno == EINTR) break;
+      perror("fgets");
       break;
     }
 
@@ -84,14 +95,29 @@ int main(int argc, char *argv[]) {
     #ifdef _WIN32
       int SEND_STATUS = send(fd, buffer, strlen(buffer), 0) == -1;
     #else
-      int SEND_STATUS = write(fd, buffer, strlen(buffer)) == -1;
+      ssize_t SEND_STATUS = write(fd, buffer, strlen(buffer)) == -1;
     #endif
-    if (SEND_STATUS) {
-      printf("Could not write data\n");
+
+    // Check if server is still connected
+    char probe;
+    ssize_t status = recv(fd, &probe, 1, MSG_PEEK | MSG_DONTWAIT);
+
+    if (status == 0) {
+        // Server closed the connection gracefully
+        printf("Server disconnected (gracefully)\n");
+        break;
+    } else if (status < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+        // Error while probing socket
+        perror("recv (checking socket state)");
+        break;
     }
-    printf("Sending msg\n");
+
+    if (SEND_STATUS < 0) {
+      perror("Could not write data");
+    }
   }
 
+  printf("Closing!");
   CLOSE(fd);
 
   return 0;
